e53dad87c6f6f5a895e9a39e293d36eb5a18bf8b
DevJoaoPeu <joaocanaldroid@gmail.com>
2025-08-21 21:13:40 -0300

    feat: config conecction with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductEntity } from './products/entities/product.entity';
 
 @Module({
-  imports: [],
+  imports: [ 
+     ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DATABASE'),
+        entities: [ProductEntity],
+        synchronize: true,
+      }),
+    }),
+    ProductsModule
+  ],
   controllers: [],
   providers: [],
 })
