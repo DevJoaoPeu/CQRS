1fb775d73d6f7eac80d3fe485bbff2ec7a27177d
DevJoaoPeu <joaocanaldroid@gmail.com>
2025-08-22 21:05:32 -0300

    feat: adding class validator

--- src/products/queries/dto/create-products.dto.ts
+++ src/products/queries/dto/create-products.dto.ts
@@ -1,7 +1,12 @@
+import { isBoolean, IsNumber, IsString } from "class-validator";
+
 export class CreateProductsDto {
+    @IsString()
     name: string;
 
+    @IsNumber()
     price: number;
     
+    @isBoolean()
     active: boolean;
 }
