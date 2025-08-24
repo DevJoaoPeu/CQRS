2a5070557f0dc52833162ec2221e5da8e5c7dbfd
DevJoaoPeu <joaocanaldroid@gmail.com>
2025-08-24 11:15:38 -0300

    feat: adding type

--- src/products/comands/impl/create-product.command.ts
+++ src/products/comands/impl/create-product.command.ts
@@ -1,7 +1,14 @@
-export class CreateProductCommand {
+import { Command } from "@nestjs/cqrs";
+import { ProductEntity } from "src/products/entities/product.entity";
+
+export class CreateProductCommand extends Command<
+  ProductEntity
+>{
   constructor(
     public readonly name: string,
     public readonly price: number,
     public readonly active: boolean
-  ) {}
+  ) {
+    super()
+  }
 }
--- src/products/queries/impl/get-products.query.ts
+++ src/products/queries/impl/get-products.query.ts
@@ -1,1 +1,4 @@
-export class GetProductsQuery {}
+import { Query } from "@nestjs/cqrs";
+import { ProductEntity } from "src/products/entities/product.entity";
+
+export class GetProductsQuery extends Query<ProductEntity[]>{}
